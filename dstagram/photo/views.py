9c09dff632f69f4fe0e1440786c8fb94442a1da1
isaccchoi <isaccchoi@naver.com>
2018-01-09 21:26:27 +0900

    post_list view에 login_required 데코레이터 적용

--- dstagram/photo/views.py
+++ dstagram/photo/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from photo.models import Photo
 
 
+@login_required
 def post_list(request):
     posts = Photo.objects.all()
     return render(request, 'photo/list.html', {"posts": posts})
