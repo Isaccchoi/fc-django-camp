1a6f197eba61f32022074aa3f5ecd5c486ed40b5
isaccchoi <isaccchoi@naver.com>
2018-01-09 21:45:46 +0900

    login logout view 구현

--- dstagram/accounts/urls.py
+++ dstagram/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^', include('django.contrib.auth.urls')),
+    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
