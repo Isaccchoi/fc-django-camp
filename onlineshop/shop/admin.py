957a36f230ec4112f2f4808b63aba84984402362
isaccchoi <isaccchoi@naver.com>
2018-01-17 22:25:17 +0900

    admin 추가

--- onlineshop/shop/admin.py
+++ onlineshop/shop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'category', 'price', 'stock', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated', 'category']
+    list_editable = ['price', 'stock', 'available']
+
+
+admin.site.register(Product, ProductAdmin)
