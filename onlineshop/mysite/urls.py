f00e8734bc3824804a479296392c7a7b0a7fe4f8
isaccchoi <isaccchoi@naver.com>
2018-01-18 20:42:45 +0900

    media root, url 추가, list, detail page 수정

--- onlineshop/mysite/urls.py
+++ onlineshop/mysite/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -6,3 +8,13 @@ urlpatterns = [
     url(r'^', include('shop.urls', namespace='shop')),
 
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+"""
+URL - View - Templates
+localhost - product_list  - list.html
+localhost/slug/ - product_list - list.html
+localhost/id/slug/ - product_detail - detail.html
+"""
