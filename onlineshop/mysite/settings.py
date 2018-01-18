2826ba360fe8fcc6c94f625727f962d338f2e112
isaccchoi <isaccchoi@naver.com>
2018-01-18 20:46:40 +0900

    static_root 지정,base에서 load

--- onlineshop/mysite/settings.py
+++ onlineshop/mysite/settings.py
@@ -118,10 +118,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+# Static
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
